d2a68b5e823aa56254bfa0fc437ac6a3bd4faa87
jakob2508 <jakob.beise@physics.uu.se>
2024-12-31 11:35:33 +0100

    added utils script to read txt files

---
+++ mdom/include/OMSimUtils.hh
@@ -0,0 +1,20 @@
+#ifndef OMSimUtils_h
+#define OMSimUtils_h
+
+#include <string>
+#include <vector>
+
+// Define the ElementData struct
+struct ElementData {
+    std::string name;  // E.g., "U238"
+    int atomicNumber;  // E.g., 92
+    int massNumber;    // E.g., 238
+    double excitationEnergy;   // in [keV] E.g., 0.0, 73.92, ...
+    int totalAngularMomentum;     // in [1/2] E.g., 0, 3, 6
+    double branchingRatio; // E.g., 1.0
+};
+
+// Declare the loadDataFromFile function
+std::vector<ElementData> loadDataFromFile(const std::string& filename);
+
+#endif // OmSimUtils_h
