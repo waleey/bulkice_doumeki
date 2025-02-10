045d3a9d3796f7640220a5170c16015af5ad23a8
Waly M Z karim <walu@dhcp-10-5-254-69.wireless.rochester.edu>
2025-02-10 17:42:05 -0500

    fixed WOM no PMT hit issue

--- mdom/env.sh
+++ mdom/env.sh
@@ -5,8 +5,8 @@
 # you may need to modify #data section too.
 #
 
-export G4BUILD=/home/waly/software/geant4-v11.1.2-install
-if [ "$G4BUILD" != "/home/waly/software/geant4-v11.1.2-install" ]; then
+export G4BUILD=/Users/walu/icecube/my_geant4/geant4-v11.0.0-install
+if [ "$G4BUILD" != "/Users/walu/icecube/my_geant4/geant4-v11.0.0-install" ]; then
     echo "Error: G4BUILD is not set to the correct path."
     exit 1  # Terminate the script with an error code
 fi #terminate the run if the installation directory isn't set up properly!
@@ -17,3 +17,6 @@ fi #terminate the run if the installation directory isn't set up properly!
 source $G4BUILD/share/Geant4/geant4make/geant4make.sh
 source $G4BUILD/bin/geant4.sh
 
+#Only uncomment these lies if on mac:
+export PKG_CONFIG_PATH=/opt/homebrew/opt/qt@5/lib/pkgconfig/
+export PATH=/opt/homebrew/opt/qt@5/bin:$PATH
---
+++ mdom/src/bits/stdc++.h
@@ -0,0 +1,19 @@
+#ifndef _GLIBCXX_BITS_STDCPP_H
+#define _GLIBCXX_BITS_STDCPP_H
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <string>
+#include <cstring>
+#include <cstdio>
+#include <cstdlib>
+#include <cassert>
+
+#endif
+
