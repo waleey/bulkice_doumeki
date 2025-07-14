778e23f3ef6ebd3436cb90b6a6c7ce926dc53ae7
slconti <saraconti225@gmail.com>
2025-07-14 15:02:39 +0000

    Fix spacing

--- merger/merge.py
+++ merger/merge.py
@@ -42,6 +42,7 @@ def bid(inputFile,chunk_num,basefolderG):
     bulkice=G4tools(args.omModel,args.simType,args.depthIndex,args.outputFolderG,f'{args.runID}{chunk_num}',inputFile,basefolderG)
     bulkice.callG4()
     print('ran bulk ice doumeki')
+
 def merge():
     tik=time.time()
     args = parseCommandLine()
