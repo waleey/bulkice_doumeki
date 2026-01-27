8c07f9e6f5b9debd683efa75598a5dc8bacf5f73
Waly M Z Karim <wkarim@cobalt-12.icecube.wisc.edu>
2026-01-27 12:37:13 -0600

    setting config

--- mdom/env.sh
+++ mdom/env.sh
@@ -5,12 +5,15 @@
 # you may need to modify #data section too.
 #
 
-export G4BUILD=/Users/walu/icecube/my_geant4/geant4-v11.0.0-install
-if [ "$G4BUILD" != "/Users/walu/icecube/my_geant4/geant4-v11.0.0-install" ]; then
-    echo "Error: G4BUILD is not set to the correct path."
-    exit 1  # Terminate the script with an error code
-fi #terminate the run if the installation directory isn't set up properly!
-#export G4BUILD=/cvmfs/icecube.opensciencegrid.org/users/hoshina/RHEL7/geant4.10.06.p02_mod_build
+#export G4UILD=/Users/walu/icecube/my_geant4/geant4-v11.0.0-install
+#if [ "$G4BUILD" != "/Users/walu/icecube/my_geant4/geant4-v11.0.0-install" ]; then
+#    echo "Error: G4BUILD is not set to the correct path."
+#    exit 1  # Terminate the script with an error code
+#fi #terminate the run if the installation directory isn't set up properly!
+eval `/cvmfs/icecube.opensciencegrid.org/py3-v4.4.0/setup.sh`
+source $ROOTSYS/bin/geant4.sh
+
+export G4BUILD=$(geant4-config --prefix)
 #export G4DATA=$G4BUILD/data/
 #export G4VIS_BUILD_RAYTRACER_DRIVER=1
 #export G4VIS_USE_RAYTRACER=1
@@ -18,8 +21,8 @@ source $G4BUILD/share/Geant4/geant4make/geant4make.sh
 source $G4BUILD/bin/geant4.sh
 
 #Only uncomment these lies if on mac:
-OS=`uname -o`
-if [[ "${OS}" == "Darwin" ]]; then
-    export PKG_CONFIG_PATH=/opt/homebrew/opt/qt@5/lib/pkgconfig/
-    export PATH=/opt/homebrew/opt/qt@5/bin:$PATH
-fi
+#OS=`uname -o`
+#if [[ "${OS}" == "Darwin" ]]; then
+ #   export PKG_CONFIG_PATH=/opt/homebrew/opt/qt@5/lib/pkgconfig/
+  #  export PATH=/opt/homebrew/opt/qt@5/bin:$PATH
+#fi
