4d747f8b8b8e905f273da561381315a2014f4032
Waly M Z karim <walu@dhcp-10-5-150-211.wireless.rochester.edu>
2026-01-27 15:51:32 -0500

    dom id fix

--- mdom/src/OMSimPositronAction.cc
+++ mdom/src/OMSimPositronAction.cc
@@ -37,7 +37,7 @@ void OMSimPositronAction::GeneratePrimaries(G4Event* anEvent)
         fParticleGun -> SetParticleEnergy(fPositronData.at(ENERGY).at(fIdx) * MeV);
         fParticleGun -> SetParticleTime(fPositronData.at(TIME).at(fIdx) * ms);
         fParticleGun -> SetParticleDefinition(G4Positron::PositronDefinition());
-        gDOMId = static_cast<int>(fPositronData.at(DOMID).at(fIdx));
+        //gDOMId = static_cast<int>(fPositronData.at(DOMID).at(fIdx));
         fParticleGun -> GeneratePrimaryVertex(anEvent);
     }
     catch(...)
