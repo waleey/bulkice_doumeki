85c3c26745750e10d7a2ca925fbd2ca2004664d1
Waly M Z karim <walu@dhcp-10-5-183-139.wireless.rochester.edu>
2026-02-03 15:16:22 -0500

    input file change

--- mdom/sleep.sh
+++ mdom/sleep.sh
@@ -12,18 +12,17 @@ cd /home/wkarim/bulkice_doumeki/mdom/build/
 source env.sh
 OUTPUT_DIR="/data/user/wkarim/output/"
 # define job number explicitly if needed
-JOBN=${PROCESS:-0}
+JOBN=${1:-0}
 
 #defining dir for shuffled data
 SHUFFLE_INPUT_DIR='/home/wkarim/bulkice_doumeki/mdom/InputFile/'
 SHUFFLE_OUTPUT_DIR='/home/wkarim/bulkice_doumeki/mdom/InputFile/Positron'
-python shuffle_data.py \
+python /home/wkarim/bulkice_doumeki/mdom/InputFile/shuffle_data.py \
     --particle Positron \
     --input-dir "$SHUFFLE_INPUT_DIR" \
-    --output-dir "SHUFFLE_OUTPUT_DIR" \
+    --output-dir "$SHUFFLE_OUTPUT_DIR" \
     --seed "$JOBN"
-    
+
 ./bulkice_doumeki mdom ibd 88 "$OUTPUT_DIR" "$JOBN"
 
 echo "Job complete!"
-
